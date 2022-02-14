33d1b1df2e1ee01d7a40fef154bbb2c18fe7b01c
Jordan Halfman <jordanhalfman@gmail.com>
2022-02-13 21:46:14 -0700

    javascript additions, heart and smash characters

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const smash = document.getElementById("smashbros");
+smash.addEventListener('click', function() {
+    alert('Luigi, Sheik, and Ness!');
+});
+
+const cats = document.getElementById("finalpic");
+const pics = document.getElementById("lastone")
+cats.addEventListener("mouseover", function(event) {
+    pics.style.zIndex = "0";
+})
+
+cats.addEventListener("mouseleave", function(event) {
+    pics.style.zIndex = "-2";
+})
